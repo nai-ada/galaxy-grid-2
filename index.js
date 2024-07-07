e9b46742be184748e08b64484522142fc1e7cd17
Nadia Vespalec <nadiavespalec00@gmail.com>
2024-07-07 00:28:51 -0700

    updated some js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Audio elements
+  // grabbing audio elements
   const lobbyMusic = document.getElementById('lobby-audio');
   const menuClickAudio = document.getElementById('menu-click-audio');
   const roleClickAudio = document.getElementById('role-click-audio');
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function () {
   // Check localStorage for mute state
   const isMuted = localStorage.getItem('isMuted') === 'true';
 
-  // Set initial mute state based on localStorage
+  // Set initial mute state based on localStorage - I have no idea how this even works the way I want it to, but it does
   [lobbyMusic, menuClickAudio, roleClickAudio].forEach((audio) => {
-    audio.muted = isMuted;
+    audio.muted = !isMuted;
   });
 
-  if (isMuted) {
+  if (!isMuted) {
     unmutedButton.style.display = 'none';
     mutedButton.style.display = 'block';
   } else {
@@ -80,3 +80,70 @@ document.addEventListener('DOMContentLoaded', function () {
     menuClickAudio.play();
   });
 });
+
+// Game logic
+
+// remembering role selector
+
+// const astronautRole = document.getElementById('astronaut-role');
+// const spaceshipRole = document.getElementById('spaceship-role');
+
+//  // Function to set a value in localStorage
+//  const setItem = (key, value) => {
+//   localStorage.setItem(key, value);
+// };
+
+// // Function to get a value from localStorage
+// const getItem = (key) => {
+//   return localStorage.getItem(key);
+// };
+
+// const playerChoseAstronautRole = () => {
+//   const playerSelectedRole = getItem('playerSelectedRole');
+//   if (playerSelectedRole) {
+
+//   } else {
+
+//   }
+// };
+
+// variable with possible win combinations
+const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// grabbing each box and creating an array from them
+const boxes = Array.from(document.getElementsByTagName('td'));
+// creating variables to set default scores for both player and cpu
+let playerScore = 0;
+let computerScore = 0;
+
+// creating variables for player and cpu roles
+let playerRole = 'P';
+let computerRole = 'C';
+
+// for every win combination in the winCombinations array, checks if each index matches our condition (which is if it contains the player's symbol)
+function checkPlayerWin() {
+  return winCombinations.some((combination) => {
+    if (
+      combination.every((index) => {
+        let td = boxes[index];
+        return td.classList.container(playerSymbol.toLowerCase());
+      })
+    ) {
+      // else if win combination IS found, add winning class to the boxes (for styling purposes, to show that the player won)
+      combination.forEach((index) => {
+        boxes[index].classList.add('won');
+      });
+      return true;
+    }
+    return false; // dont execute this code if player does not win
+  });
+}
