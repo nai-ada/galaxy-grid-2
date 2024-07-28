3f809894af3fa0bd30d3e6fa555713791ddfee90
Nadia Vespalec <nadiavespalec00@gmail.com>
2024-07-28 12:24:00 -0700

    added some logic for audio

--- index.js
+++ index.js
@@ -81,69 +81,56 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-// Game logic
+// Game Logic
+// document.querySelectorAll('td').forEach((cell) => {
+//   cell.addEventListener('click', handleClick, { once: true });
+// });
 
-// remembering role selector
+// let currentPlayer = 'X';
 
-// const astronautRole = document.getElementById('astronaut-role');
-// const spaceshipRole = document.getElementById('spaceship-role');
-
-//  // Function to set a value in localStorage
-//  const setItem = (key, value) => {
-//   localStorage.setItem(key, value);
-// };
-
-// // Function to get a value from localStorage
-// const getItem = (key) => {
-//   return localStorage.getItem(key);
-// };
-
-// const playerChoseAstronautRole = () => {
-//   const playerSelectedRole = getItem('playerSelectedRole');
-//   if (playerSelectedRole) {
-
-//   } else {
-
-//   }
-// };
+// function handleClick(event) {
+//   const cell = event.target;
+//   cell.innerHTML = currentPlayer;
+//   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+// }
 
 // variable with possible win combinations
-const winCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+// const winCombinations = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
 
-// grabbing each box and creating an array from them
-const boxes = Array.from(document.getElementsByTagName('td'));
-// creating variables to set default scores for both player and cpu
-let playerScore = 0;
-let computerScore = 0;
+// // grabbing each box and creating an array from them
+// const boxes = Array.from(document.getElementsByTagName('td'));
+// // creating variables to set default scores for both player and cpu
+// let playerScore = 0;
+// let computerScore = 0;
 
-// creating variables for player and cpu roles
-let playerRole = 'P';
-let computerRole = 'C';
+// // creating variables for player and cpu roles
+// let playerRole = 'P';
+// let computerRole = 'C';
 
-// for every win combination in the winCombinations array, checks if each index matches our condition (which is if it contains the player's symbol)
-function checkPlayerWin() {
-  return winCombinations.some((combination) => {
-    if (
-      combination.every((index) => {
-        let td = boxes[index];
-        return td.classList.container(playerSymbol.toLowerCase());
-      })
-    ) {
-      // else if win combination IS found, add winning class to the boxes (for styling purposes, to show that the player won)
-      combination.forEach((index) => {
-        boxes[index].classList.add('won');
-      });
-      return true;
-    }
-    return false; // dont execute this code if player does not win
-  });
-}
+// // for every win combination in the winCombinations array, checks if each index matches our condition (which is if it contains the player's symbol)
+// function checkPlayerWin() {
+//   return winCombinations.some((combination) => {
+//     if (
+//       combination.every((index) => {
+//         let td = boxes[index];
+//         return td.classList.container(playerSymbol.toLowerCase());
+//       })
+//     ) {
+//       // else if win combination IS found, add winning class to the boxes (for styling purposes, to show that the player won)
+//       combination.forEach((index) => {
+//         boxes[index].classList.add('won');
+//       });
+//       return true;
+//     }
+//     return false; // dont execute this code if player does not win
+//   });
+// }
